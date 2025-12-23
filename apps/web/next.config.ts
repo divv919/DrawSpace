2cdb17b7a2c592c3c1e9bc69e4395d8b29e3aca3
divv919 <divyanshsoni919@gmail.com>
2025-12-23 14:38:47 +0530

    fix: prisma nextjs workaround install and setup

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
-
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["vercel.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
   },
 };
 
