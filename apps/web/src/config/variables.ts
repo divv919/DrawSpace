f056f3ac9ad624a4e6908dd99e39ad7128d9c10a
divv919 <divyanshsoni919@gmail.com>
2025-12-23 14:54:43 +0530

    fix: rename env variables in nextjs

--- apps/web/src/config/variables.ts
+++ apps/web/src/config/variables.ts
@@ -1,4 +1,5 @@
-const BACKEND_BASE_URL = process.env.HTTP_URL || "http://localhost:3002";
-const WEBSOCKET_URL = process.env.WS_URL || "ws://localhost:8080";
+const BACKEND_BASE_URL =
+  process.env.NEXT_PUBLIC_HTTP_URL || "http://localhost:3002";
+const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
 
 export { BACKEND_BASE_URL, WEBSOCKET_URL };
